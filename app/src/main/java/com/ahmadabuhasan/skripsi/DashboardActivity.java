adccd648f07b6bdb7eb76b220e876fdf3b377f26
eby8zevin <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2021-01-01 05:44:41 +0700

    add DashboardActivity

--- app/src/main/java/com/ahmadabuhasan/skripsi/DashboardActivity.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/DashboardActivity.java
@@ -1,14 +1,138 @@
 package com.ahmadabuhasan.skripsi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import com.ahmadabuhasan.skripsi.data.ProductActivity;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+
+import es.dmoral.toasty.Toasty;
+
+/*
+ * Created by Ahmad Abu Hasan on 01/01/2021
+ */
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private static long back_pressed;
+
+    CardView cardView_kaca;
+    CardView cardView_pigura;
+    CardView cardView_kasir;
+    CardView cardView_data;
+    CardView cardView_print;
+    CardView cardView_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        this.cardView_kaca = findViewById(R.id.card_kaca);
+        this.cardView_pigura = findViewById(R.id.card_pigura);
+        this.cardView_kasir = findViewById(R.id.card_kasir);
+        this.cardView_data = findViewById(R.id.card_data);
+        this.cardView_print = findViewById(R.id.card_print);
+        this.cardView_settings = findViewById(R.id.card_settings);
+
+        this.cardView_data.setOnClickListener(v -> DashboardActivity.this.startActivity(new Intent(DashboardActivity.this, ProductActivity.class)));
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermission();
+        }
+    }
+
+    public void onBackPressed() {
+        if (back_pressed + 2000 > System.currentTimeMillis()) {
+            finishAffinity();
+        } else {
+            Toasty.info((Context) this, (int) R.string.press_once_again_to_exit, 0).show();
+        }
+        back_pressed = System.currentTimeMillis();
+    }
+
+    /*
+     * Requesting multiple permissions (storage and location) at once
+     * This uses multiple permission model from dexter
+     * On permanent denial opens settings dialog
+     */
+    private void requestPermission() {
+        Dexter.withContext(this)
+                .withPermissions(
+                        "android.permission.CAMERA",
+                        "android.permission.WRITE_EXTERNAL_STORAGE",
+                        "android.permission.READ_EXTERNAL_STORAGE")
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(getApplicationContext(), "All permissions are granted!", Toast.LENGTH_SHORT).show();
+                        }
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            showSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).withErrorListener(new PermissionRequestErrorListener() {
+            @Override
+            public void onError(DexterError error) {
+                Toast.makeText(DashboardActivity.this.getApplicationContext(), R.string.error, Toast.LENGTH_SHORT).show();
+            }
+        }).onSameThread().check();
+    }
+
+    /*
+     * Showing Alert Dialog with Settings option
+     * Navigates user to app settings
+     * NOTE: Keep proper title and message depending on your app
+     */
+    private void showSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(DashboardActivity.this);
+        builder.setTitle("Need Permissions");
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
+        builder.setPositiveButton("GOTO SETTINGS", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                openSettings();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+
+    // navigating user to app settings
+    private void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, 101);
     }
 }
