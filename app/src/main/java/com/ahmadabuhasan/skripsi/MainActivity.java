f71dae8a0845a254435241b0296dbc2f90a81c94
eby8zevin <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2020-12-31 22:04:06 +0700

    Skripsi

--- app/src/main/java/com/ahmadabuhasan/skripsi/MainActivity.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * Created by Ahmad Abu Hasan on 31/12/2020
+ * "com.android.tools.build:gradle:4.1.1"
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
