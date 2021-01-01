ed743e684254a580e3b3f48387c360771330bafe
eby8zevin <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2021-01-02 03:21:44 +0700

    add EditProductActivity

---
+++ app/src/main/java/com/ahmadabuhasan/skripsi/data/EditProductActivity.java
@@ -0,0 +1,16 @@
+package com.ahmadabuhasan.skripsi.data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.skripsi.R;
+
+public class EditProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_product);
+    }
+}
