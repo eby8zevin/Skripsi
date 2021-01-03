9fdbfb2809433684a0c1828b1a40c607b28fbd85
eby8zevin <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2021-01-03 17:00:21 +0700

    add ShopInformationActivity

---
+++ app/src/main/java/com/ahmadabuhasan/skripsi/settings/shop/ShopInformationActivity.java
@@ -0,0 +1,16 @@
+package com.ahmadabuhasan.skripsi.settings.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.skripsi.R;
+
+public class ShopInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_information);
+    }
+}
