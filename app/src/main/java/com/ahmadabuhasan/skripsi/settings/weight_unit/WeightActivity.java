88d1534bee9ac6eed2e23caa8461f8fc6fdf1870
eby8zevin <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2021-01-05 23:40:32 +0700

    add WeightActivity

---
+++ app/src/main/java/com/ahmadabuhasan/skripsi/settings/weight_unit/WeightActivity.java
@@ -0,0 +1,113 @@
+package com.ahmadabuhasan.skripsi.settings.weight_unit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.ahmadabuhasan.skripsi.R;
+import com.ahmadabuhasan.skripsi.adapter.WeightAdapter;
+import com.ahmadabuhasan.skripsi.database.DatabaseAccess;
+import com.ahmadabuhasan.skripsi.settings.SettingsActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.HashMap;
+import java.util.List;
+
+import es.dmoral.toasty.Toasty;
+
+/*
+ * Created by Ahmad Abu Hasan on 05/01/2021
+ */
+
+public class WeightActivity extends AppCompatActivity {
+
+    EditText editText_Search;
+    FloatingActionButton floatingActionButton_fabAdd;
+    ImageView imgNoProduct;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.weight_unit);
+
+        this.editText_Search = findViewById(R.id.et_weight_search);
+        this.floatingActionButton_fabAdd = findViewById(R.id.fab_add);
+        this.imgNoProduct = findViewById(R.id.image_no_product);
+        this.recyclerView = findViewById(R.id.weight_recyclerview);
+
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        this.recyclerView.setHasFixedSize(true);
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        List<HashMap<String, String>> weightData = databaseAccess.getWeightUnit();
+        Log.d("data", "" + weightData.size());
+
+        if (weightData.size() <= 0) {
+            Toasty.info(this, (int) R.string.no_data_found, Toasty.LENGTH_SHORT).show();
+            this.imgNoProduct.setImageResource(R.drawable.no_data);
+        } else {
+            this.imgNoProduct.setVisibility(View.GONE);
+            this.recyclerView.setAdapter(new WeightAdapter(this, weightData));
+        }
+
+        this.floatingActionButton_fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WeightActivity.this.startActivity(new Intent(WeightActivity.this, AddWeightActivity.class));
+            }
+        });
+
+        this.editText_Search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                DatabaseAccess databaseAccess = DatabaseAccess.getInstance(WeightActivity.this);
+                databaseAccess.open();
+                List<HashMap<String, String>> searchWeightList = databaseAccess.searchProductWeight(s.toString());
+                if (searchWeightList.size() <= 0) {
+                    WeightActivity.this.recyclerView.setVisibility(View.GONE);
+                    WeightActivity.this.imgNoProduct.setVisibility(View.VISIBLE);
+                    WeightActivity.this.imgNoProduct.setImageResource(R.drawable.no_data);
+                    return;
+                }
+                WeightActivity.this.recyclerView.setVisibility(View.VISIBLE);
+                WeightActivity.this.imgNoProduct.setVisibility(View.GONE);
+                WeightActivity.this.recyclerView.setAdapter(new WeightAdapter(WeightActivity.this, searchWeightList));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
