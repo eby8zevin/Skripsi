de2ac04ef3c206052457649afda07823a753de58
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-06 18:08:53 +0700

    add EditSuppliersActivity

---
+++ app/src/main/java/com/ahmadabuhasan/skripsi/suppliers/EditSuppliersActivity.java
@@ -0,0 +1,16 @@
+package com.ahmadabuhasan.skripsi.suppliers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.skripsi.R;
+
+public class EditSuppliersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_suppliers);
+    }
+}
