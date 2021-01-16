6c740ef1bd0db66f199dac4fc9597ff5feaaf3c1
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-16 11:09:19 +0700

    add CustomerAdapter

--- app/src/main/java/com/ahmadabuhasan/skripsi/adapter/CustomerAdapter.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/adapter/CustomerAdapter.java
@@ -26,7 +26,7 @@ import java.util.List;
 import es.dmoral.toasty.Toasty;
 
 /*
- * Created by Ahmad Abu Hasan on 13/01/2021
+ * Created by Ahmad Abu Hasan on 16/01/2021
  */
 
 public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.MyViewHolder> {
@@ -47,20 +47,21 @@ public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.MyView
 
     public void onBindViewHolder(final MyViewHolder holder, int position) {
         final String customer_id = this.customerData.get(position).get(DatabaseOpenHelper.CUSTOMER_ID);
-        final String cell = this.customerData.get(position).get(DatabaseOpenHelper.CUSTOMER_CALL);
+        final String hp = this.customerData.get(position).get(DatabaseOpenHelper.CUSTOMER_HP);
 
-        holder.txtCustomerName.setText(this.customerData.get(position).get(DatabaseOpenHelper.CUSTOMER_NAME));
-        holder.txtCell.setText(cell);
-        holder.txtEmail.setText(this.customerData.get(position).get(DatabaseOpenHelper.CUSTOMER_EMAIL));
-        holder.txtAddress.setText(this.customerData.get(position).get(DatabaseOpenHelper.CUSTOMER_ADDRESS));
+        holder.textView_CustomerName.setText(this.customerData.get(position).get(DatabaseOpenHelper.CUSTOMER_NAME));
+        holder.textView_Address.setText(this.customerData.get(position).get(DatabaseOpenHelper.CUSTOMER_ADDRESS));
+        holder.textView_Hp.setText(hp);
+        holder.textView_Wa.setText(this.customerData.get(position).get(DatabaseOpenHelper.CUSTOMER_WA));
+
         holder.imgCall.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View v) {
                 Intent callIntent = new Intent("android.intent.action.DIAL");
-                callIntent.setData(Uri.parse("tel:" + cell));
+                callIntent.setData(Uri.parse("tel:" + hp));
                 CustomerAdapter.this.context.startActivity(callIntent);
             }
         });
+
         holder.imgDelete.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 new AlertDialog.Builder(CustomerAdapter.this.context).setMessage(R.string.want_to_delete_customer).setCancelable(false).setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
@@ -91,21 +92,22 @@ public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.MyView
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView textView_CustomerName;
+        TextView textView_Address;
+        TextView textView_Hp;
+        TextView textView_Wa;
+
         ImageView imgCall;
         ImageView imgDelete;
-        TextView txtAddress;
-        TextView txtCell;
-        TextView txtCustomerName;
-        TextView txtEmail;
 
         public MyViewHolder(View itemView) {
             super(itemView);
-            this.txtCustomerName = (TextView) itemView.findViewById(R.id.txt_customer_name);
-            this.txtCell = (TextView) itemView.findViewById(R.id.txt_cell);
-            this.txtEmail = (TextView) itemView.findViewById(R.id.txt_email);
-            this.txtAddress = (TextView) itemView.findViewById(R.id.txt_address);
-            this.imgDelete = (ImageView) itemView.findViewById(R.id.img_delete);
-            this.imgCall = (ImageView) itemView.findViewById(R.id.img_call);
+            this.textView_CustomerName = itemView.findViewById(R.id.tv_customer_name);
+            this.textView_Address = itemView.findViewById(R.id.tv_customer_address);
+            this.textView_Hp = itemView.findViewById(R.id.tv_customer_hp);
+            this.textView_Wa = itemView.findViewById(R.id.tv_customer_wa);
+            this.imgDelete = itemView.findViewById(R.id.img_delete);
+            this.imgCall = itemView.findViewById(R.id.img_call);
             itemView.setOnClickListener(this);
         }
 
@@ -113,9 +115,12 @@ public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.MyView
             Intent i = new Intent(CustomerAdapter.this.context, EditCustomersActivity.class);
             i.putExtra(DatabaseOpenHelper.CUSTOMER_ID, (String) ((HashMap) CustomerAdapter.this.customerData.get(getAdapterPosition())).get(DatabaseOpenHelper.CUSTOMER_ID));
             i.putExtra(DatabaseOpenHelper.CUSTOMER_NAME, (String) ((HashMap) CustomerAdapter.this.customerData.get(getAdapterPosition())).get(DatabaseOpenHelper.CUSTOMER_NAME));
-            i.putExtra(DatabaseOpenHelper.CUSTOMER_CALL, (String) ((HashMap) CustomerAdapter.this.customerData.get(getAdapterPosition())).get(DatabaseOpenHelper.CUSTOMER_CALL));
-            i.putExtra(DatabaseOpenHelper.CUSTOMER_EMAIL, (String) ((HashMap) CustomerAdapter.this.customerData.get(getAdapterPosition())).get(DatabaseOpenHelper.CUSTOMER_EMAIL));
             i.putExtra(DatabaseOpenHelper.CUSTOMER_ADDRESS, (String) ((HashMap) CustomerAdapter.this.customerData.get(getAdapterPosition())).get(DatabaseOpenHelper.CUSTOMER_ADDRESS));
+            i.putExtra(DatabaseOpenHelper.CUSTOMER_HP, (String) ((HashMap) CustomerAdapter.this.customerData.get(getAdapterPosition())).get(DatabaseOpenHelper.CUSTOMER_HP));
+            i.putExtra(DatabaseOpenHelper.CUSTOMER_WA, (String) ((HashMap) CustomerAdapter.this.customerData.get(getAdapterPosition())).get(DatabaseOpenHelper.CUSTOMER_WA));
+            i.putExtra(DatabaseOpenHelper.CUSTOMER_ACCOUNT, (String) ((HashMap) CustomerAdapter.this.customerData.get(getAdapterPosition())).get(DatabaseOpenHelper.CUSTOMER_ACCOUNT));
+            i.putExtra(DatabaseOpenHelper.CUSTOMER_INFORMATION, (String) ((HashMap) CustomerAdapter.this.customerData.get(getAdapterPosition())).get(DatabaseOpenHelper.CUSTOMER_INFORMATION));
+            i.putExtra(DatabaseOpenHelper.CUSTOMER_LAST_UPDATE, (String) ((HashMap) CustomerAdapter.this.customerData.get(getAdapterPosition())).get(DatabaseOpenHelper.CUSTOMER_LAST_UPDATE));
             CustomerAdapter.this.context.startActivity(i);
         }
     }
