c7e85c7dc9e732c3a8f67bdbcfcf327ab7beda27
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-17 18:06:14 +0700

    add DatabaseAccess

--- app/src/main/java/com/ahmadabuhasan/skripsi/database/DatabaseAccess.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/database/DatabaseAccess.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /*
- * Created by Ahmad Abu Hasan on 16/01/2021
+ * Created by Ahmad Abu Hasan on 17/01/2021
  */
 
 public class DatabaseAccess {
@@ -603,6 +603,66 @@ public class DatabaseAccess {
         long check = (long) this.database.update("suppliers", values, "supplier_id=?", new String[]{suppliers_id});
         this.database.close();
         return check != -1;
+    }
+
+    // PosProductAdapter
+    public int addToCart(String product_id, String weight, String weight_unit, String price, int qty, String stock) {
+        SQLiteDatabase sQLiteDatabase = this.database;
+        if (sQLiteDatabase.rawQuery("SELECT * FROM product_cart WHERE product_id='" + product_id + "'", null).getCount() >= 1) {
+            return 2;
+        }
+        ContentValues values = new ContentValues();
+        values.put(DatabaseOpenHelper.CART_PRODUCT_ID, product_id);
+        values.put(DatabaseOpenHelper.CART_PRODUCT_WEIGHT, weight);
+        values.put(DatabaseOpenHelper.CART_PRODUCT_WEIGHT_UNIT, weight_unit);
+        values.put(DatabaseOpenHelper.CART_PRODUCT_PRICE, price);
+        values.put(DatabaseOpenHelper.CART_PRODUCT_QTY, qty);
+        values.put(DatabaseOpenHelper.CART_PRODUCT_STOCK, stock);
+        long check = this.database.insert("product_cart", null, values);
+        this.database.close();
+        if (check == -1) {
+            return -1;
+        }
+        return 1;
+    }
+
+    // PosProductAdapter
+    public int getCartItemCount() {
+        Cursor cursor = this.database.rawQuery("SELECT * FROM product_cart", null);
+        int itemCount = cursor.getCount();
+        cursor.close();
+        this.database.close();
+        return itemCount;
+    }
+
+    // ProductCategoryAdapter
+    public ArrayList<HashMap<String, String>> getTabProducts(String category_id) {
+        ArrayList<HashMap<String, String>> product = new ArrayList<>();
+        SQLiteDatabase sQLiteDatabase = this.database;
+        Cursor cursor = sQLiteDatabase.rawQuery("SELECT * FROM products WHERE product_category = '" + category_id + "' ORDER BY product_id DESC", (String[]) null);
+        if (cursor.moveToFirst()) {
+            do {
+                HashMap<String, String> map = new HashMap<>();
+                map.put(DatabaseOpenHelper.PRODUCT_ID, cursor.getString(0));
+                map.put(DatabaseOpenHelper.PRODUCT_NAME, cursor.getString(1));
+                map.put(DatabaseOpenHelper.PRODUCT_CODE, cursor.getString(2));
+                map.put(DatabaseOpenHelper.PRODUCT_CATEGORY, cursor.getString(3));
+                map.put(DatabaseOpenHelper.PRODUCT_BUY, cursor.getString(4));
+                map.put(DatabaseOpenHelper.PRODUCT_STOCK, cursor.getString(5));
+                map.put(DatabaseOpenHelper.PRODUCT_PRICE, cursor.getString(6));
+                map.put(DatabaseOpenHelper.PRODUCT_TOTAL_QTY, cursor.getString(7));
+                map.put(DatabaseOpenHelper.PRODUCT_DISC_QTY, cursor.getString(8));
+                map.put(DatabaseOpenHelper.PRODUCT_WEIGHT, cursor.getString(9));
+                map.put(DatabaseOpenHelper.PRODUCT_WEIGHT_UNIT_ID, cursor.getString(10));
+                map.put(DatabaseOpenHelper.PRODUCT_LAST_UPDATE, cursor.getString(11));
+                map.put(DatabaseOpenHelper.PRODUCT_INFORMATION, cursor.getString(12));
+                map.put(DatabaseOpenHelper.PRODUCT_SUPPLIER, cursor.getString(13));
+                product.add(map);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        this.database.close();
+        return product;
     }
 
 
