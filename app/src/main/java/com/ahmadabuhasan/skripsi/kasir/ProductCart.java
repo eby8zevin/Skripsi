c7703925f003784bdc6c83592346d3b3f870e7d2
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-17 18:05:28 +0700

    add ProductCart

---
+++ app/src/main/java/com/ahmadabuhasan/skripsi/kasir/ProductCart.java
@@ -0,0 +1,16 @@
+package com.ahmadabuhasan.skripsi.kasir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.skripsi.R;
+
+public class ProductCart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_cart);
+    }
+}
