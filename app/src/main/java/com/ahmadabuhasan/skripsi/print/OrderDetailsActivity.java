a494a61493ef7ed8c82945c485a5f67733138a12
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-24 09:18:39 +0700

    add OrderDetailsActivity

---
+++ app/src/main/java/com/ahmadabuhasan/skripsi/print/OrderDetailsActivity.java
@@ -0,0 +1,20 @@
+package com.ahmadabuhasan.skripsi.print;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.skripsi.R;
+
+/*
+ * Created by Ahmad Abu Hasan on 24/01/2021
+ */
+
+public class OrderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+    }
+}
