ea9619da32b15549696d5ae52a6276c9cec62c37
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-26 02:45:11 +0700

    add ExpenseActivity

---
+++ app/src/main/java/com/ahmadabuhasan/skripsi/expense/ExpenseActivity.java
@@ -0,0 +1,16 @@
+package com.ahmadabuhasan.skripsi.expense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.skripsi.R;
+
+public class ExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense);
+    }
+}
