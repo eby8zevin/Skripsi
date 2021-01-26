3785f133c81b8e9bd68e43428c036a65c480f7eb
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-27 05:57:17 +0700

    add SalesReportActivity

---
+++ app/src/main/java/com/ahmadabuhasan/skripsi/report/SalesReportActivity.java
@@ -0,0 +1,20 @@
+package com.ahmadabuhasan.skripsi.report;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.skripsi.R;
+
+/*
+ * Created by Ahmad Abu Hasan on 27/01/2021
+ */
+
+public class SalesReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales_report);
+    }
+}
