10fc745771ce70b210b4db14c8605b4aed70c903
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-30 16:23:55 +0700

    add AddPaymentMethodActivity

--- app/src/main/java/com/ahmadabuhasan/skripsi/settings/payment_method/AddPaymentMethodActivity.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/settings/payment_method/AddPaymentMethodActivity.java
@@ -2,17 +2,25 @@ package com.ahmadabuhasan.skripsi.settings.payment_method;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.ahmadabuhasan.skripsi.R;
+import com.ahmadabuhasan.skripsi.database.DatabaseAccess;
 //import com.itextpdf.text.io.PagedChannelRandomAccessSource;
+
+import es.dmoral.toasty.Toasty;
 
 /*
  * Created by Ahmad Abu Hasan on 30/01/2021
  */
 
 public class AddPaymentMethodActivity extends AppCompatActivity {
-    
+
     EditText editText_PaymentMethod;
     TextView textView_Add;
 
@@ -20,14 +28,14 @@ public class AddPaymentMethodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_payment_method);
-        
+
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.add_payment_method);
-        
+
         this.editText_PaymentMethod = findViewById(R.id.et_payment_method_name);
         this.textView_Add = findViewById(R.id.tv_add_payment_method);
-        
+
         this.textView_Add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,9 +57,8 @@ public class AddPaymentMethodActivity extends AppCompatActivity {
                 Toasty.error(AddPaymentMethodActivity.this, (int) R.string.failed, Toasty.LENGTH_SHORT).show();
             }
         });
-        
     }
-    
+
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() != android.R.id.home) {
             return super.onOptionsItemSelected(item);
