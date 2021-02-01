9005e6d80cf8dc207b92759300d16a789e08ca0b
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-02-01 23:45:51 +0700

    add IBixolonCanvasConst

---
+++ app/src/main/java/com/ahmadabuhasan/skripsi/utils/IBixolonCanvasConst.java
@@ -0,0 +1,17 @@
+package com.ahmadabuhasan.skripsi.utils;
+
+/*
+ * Created by Ahmad Abu Hasan on 01/02/2021
+ */
+
+//https://github.com/saeed-khalafinejad/Android-Thermal-Bluetooth-Print
+public interface IBixolonCanvasConst {
+
+    int CCharWidth = 16;
+    int CCharHeight = 16;
+    int CMaxChars_2Inch = 30;
+    int CMaxChars_4Inch = 52;
+    int CMaxChars_3Inch = 36;
+    int CTableWidth4Inch = 818;//370;
+    int CTableWidth3Inch = 562;
+}
