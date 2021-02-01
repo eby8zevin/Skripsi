90bedacefabaebb9f1b835ea172f552ebd1b6bf3
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-02-01 23:50:38 +0700

    add WCharMapperCT42BNazanin

---
+++ app/src/main/java/com/ahmadabuhasan/skripsi/utils/WCharMapperCT42BNazanin.java
@@ -0,0 +1,10 @@
+package com.ahmadabuhasan.skripsi.utils;
+
+/*
+ * Created by Ahmad Abu Hasan on 01/02/2021
+ */
+
+//https://github.com/saeed-khalafinejad/Android-Thermal-Bluetooth-Print
+public class WCharMapperCT42BNazanin extends WCharMapperCT41 {
+
+}
