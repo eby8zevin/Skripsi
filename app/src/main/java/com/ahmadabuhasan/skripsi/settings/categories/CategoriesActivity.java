840918ca2d2cc9f563a3b36793971299fcc22d53
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-02-05 21:45:15 +0700

    add CategoriesActivity.java

--- app/src/main/java/com/ahmadabuhasan/skripsi/settings/categories/CategoriesActivity.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/settings/categories/CategoriesActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -26,7 +27,7 @@ import java.util.List;
 import es.dmoral.toasty.Toasty;
 
 /*
- * Created by Ahmad Abu Hasan on 28/01/2021
+ * Created by Ahmad Abu Hasan on 05/02/2021
  */
 
 public class CategoriesActivity extends AppCompatActivity {
@@ -44,6 +45,7 @@ public class CategoriesActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.categories);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         this.editText_Search = findViewById(R.id.et_categories_search);
         this.floatingActionButton_fabAdd = findViewById(R.id.fab_add);
