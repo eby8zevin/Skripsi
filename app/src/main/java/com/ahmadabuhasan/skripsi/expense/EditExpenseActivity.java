837a44cfbf49f6967d7590d7d4ec8ae8a5354d11
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-02-05 21:07:55 +0700

    add EditExpenseActivity.java

--- app/src/main/java/com/ahmadabuhasan/skripsi/expense/EditExpenseActivity.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/expense/EditExpenseActivity.java
@@ -7,6 +7,7 @@ import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,7 +26,7 @@ import java.util.Calendar;
 import es.dmoral.toasty.Toasty;
 
 /*
- * Created by Ahmad Abu Hasan on 28/01/2021
+ * Created by Ahmad Abu Hasan on 05/02/2021
  */
 
 public class EditExpenseActivity extends AppCompatActivity {
@@ -53,6 +54,7 @@ public class EditExpenseActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.edit_expense);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         this.editText_ExpenseName = findViewById(R.id.et_expense_name);
         this.editText_ExpenseNote = findViewById(R.id.et_expense_note);
