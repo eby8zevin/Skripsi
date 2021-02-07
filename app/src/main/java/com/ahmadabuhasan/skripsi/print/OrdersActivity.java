82ab5b3a4d533d45f8fa120346220023b7a7b503
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-02-07 21:51:28 +0700

    add OrdersActivity.java

--- app/src/main/java/com/ahmadabuhasan/skripsi/print/OrdersActivity.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/print/OrdersActivity.java
@@ -11,6 +11,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,7 +27,7 @@ import java.util.List;
 import es.dmoral.toasty.Toasty;
 
 /*
- * Created by Ahmad Abu Hasan on 05/02/2021
+ * Created by Ahmad Abu Hasan on 07/02/2021
  */
 
 public class OrdersActivity extends AppCompatActivity {
@@ -46,6 +47,7 @@ public class OrdersActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.order_history);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         this.editText_Search = findViewById(R.id.et_search_order);
         this.textView_NoProducts = findViewById(R.id.tv_no_order);
