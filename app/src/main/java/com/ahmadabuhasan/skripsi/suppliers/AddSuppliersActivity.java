47dd3b60e4a5bea7321a25ed4bd1555e2a20813c
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-02-07 22:00:00 +0700

    add AddSuppliersActivity.java

--- app/src/main/java/com/ahmadabuhasan/skripsi/suppliers/AddSuppliersActivity.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/suppliers/AddSuppliersActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,7 +34,7 @@ import java.util.Calendar;
 import es.dmoral.toasty.Toasty;
 
 /*
- * Created by Ahmad Abu Hasan on 05/02/2021
+ * Created by Ahmad Abu Hasan on 07/02/2021
  */
 
 public class AddSuppliersActivity extends AppCompatActivity {
@@ -64,6 +65,7 @@ public class AddSuppliersActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.add_suppliers);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         this.editText_Name = findViewById(R.id.et_supplier_name);
         this.editText_Address = findViewById(R.id.et_supplier_address);
