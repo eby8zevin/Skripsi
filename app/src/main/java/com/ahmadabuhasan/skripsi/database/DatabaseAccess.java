bbf3fcf69e4475570295b6dd118ec4871f839aba
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-03-16 14:52:09 +0700

    Update DatabaseAccess.java

--- app/src/main/java/com/ahmadabuhasan/skripsi/database/DatabaseAccess.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/database/DatabaseAccess.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.util.Locale;
 
 /*
- * Created by Ahmad Abu Hasan on 15/03/2021
+ * Created by Ahmad Abu Hasan on 16/03/2021
  */
 
 public class DatabaseAccess {
