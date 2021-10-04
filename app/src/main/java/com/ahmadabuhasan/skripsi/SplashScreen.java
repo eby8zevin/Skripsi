7b9d4bc31d492d398ac88524a3202ce68adcd4c2
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-10-04 22:35:51 +0700

    add SplashScreen

--- app/src/main/java/com/ahmadabuhasan/skripsi/SplashScreen.java
+++ app/src/main/java/com/ahmadabuhasan/skripsi/SplashScreen.java
@@ -1,15 +1,15 @@
 package com.ahmadabuhasan.skripsi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.auth.api.credentials.CredentialsApi;
 
 /*
- * Created by Ahmad Abu Hasan on 17/01/2021
+ * Created by Ahmad Abu Hasan on 04/10/2021
  */
 
 public class SplashScreen extends AppCompatActivity {
@@ -25,7 +25,7 @@ public class SplashScreen extends AppCompatActivity {
 
         new Handler().postDelayed(new Runnable() {
             public void run() {
-                SplashScreen.this.startActivity(new Intent(SplashScreen.this, DashboardActivity.class));
+                SplashScreen.this.startActivity(new Intent(SplashScreen.this, LoginActivity.class));
                 SplashScreen.this.finish();
             }
         }, (long) splashTimeOut);
